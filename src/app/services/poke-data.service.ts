4165adba6afba1eb85f1c53a1dc0bdf0a49915a1
ShocikI <szotmicha16@gmail.com>
2024-11-25 20:15:48 +0100

    secrets menagement

--- src/app/services/poke-data.service.ts
+++ src/app/services/poke-data.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeDataService {
-  private apiUrl = 'https://pokeapi.co/api/v2/pokemon'
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+// This file and whole folder environments should be in .gitignore
+// to not commit secrets to github
+export const environment = {
+    production: false,
+    apiUrl: 'https://pokeapi.co/api/v2/pokemon'
+};
